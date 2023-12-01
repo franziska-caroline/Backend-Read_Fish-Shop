7839c7aefcfeab1525fe382795236b1c1c089dad
Franziska Adam <franziskacarolineadam@gmail.com>
2023-12-01 15:32:02 +0100

    Add a POST route

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -5,13 +5,25 @@ export default async function handler(request, response) {
   // to connect to the database
   await dbConnect();
 
-  // check if the request method is GET
+  // request method GET
   if (request.method === "GET") {
     // make a request to the db to get the jokes
     const products = await Product.find();
-
     console.log("products: ", products);
-
     return response.status(200).json(products);
+  }
+
+  // try...catch block
+  // request method POST
+  if (request.method === "POST") {
+    try {
+      const productData = request.body; // product.Data submitted by the form - accessible in request.body
+      await Product.create(productData); // create a new document in our collection
+
+      response.status(201).json({ status: "You added a new fish!🐠" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   }
 }
