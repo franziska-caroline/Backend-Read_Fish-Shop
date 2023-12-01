fcd3b22cbeabf19ad0d603ad7ef77ab600b28375
Franziska Adam <franziskacarolineadam@gmail.com>
2023-12-01 11:51:36 +0100

    Solve Task: Read Products from Database

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,5 +1,17 @@
-import { products } from "../../../lib/products";
+import dbConnect from "../../../db/connect";
+import Product from "../../../db/models/Product";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+export default async function handler(request, response) {
+  // to connect to the database
+  await dbConnect();
+
+  // check if the request method is GET
+  if (request.method === "GET") {
+    // make a request to the db to get the jokes
+    const products = await Product.find();
+
+    console.log("products: ", products);
+
+    return response.status(200).json(products);
+  }
 }
